393739b4efbd3f94756b0b7d8e5de2b57a62bbc2
Vvchick <mafios3210@gmail.com>
2023-09-23 01:06:45 +0300

    first try

---
+++ build/CMakeFiles/lab01.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab01.dir/lab01.c.o"
+  "CMakeFiles/lab01.dir/lab01.c.o.d"
+  "lab01"
+  "lab01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/vladimirmarkov/repos/mat_prak_lab
+# Build directory: /Users/vladimirmarkov/repos/mat_prak_lab/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
